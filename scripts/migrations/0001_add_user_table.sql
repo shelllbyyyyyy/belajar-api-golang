ac8be79ff7bc38ddc28ce3e697d9ed0f605dddb6
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-12-19 10:37:42 +0700

    chore: add user migration

---
+++ scripts/migrations/0001_add_user_table.sql
@@ -0,0 +1,12 @@
+CREATE TYPE IF NOT EXISTS public.roles AS ENUM ('user', 'admin');
+
+CREATE TABLE IF NOT EXISTS public.users (
+    id uuid PRIMARY KEY NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role public.roles DEFAULT 'user',
+    is_deleted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
